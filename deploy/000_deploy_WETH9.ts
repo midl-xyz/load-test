5a830158a215c489b55091a1c35f35bc1417bd01
Alex Novak <heyalexnovak@gmail.com>
2025-12-05 17:24:06 +0000

    feat: update midljs

--- deploy/000_deploy_WETH9.ts
+++ deploy/000_deploy_WETH9.ts
@@ -1,6 +1,14 @@
+import { configure, getConsoleSink } from "@logtape/logtape";
 import type { DeployFunction } from "hardhat-deploy/types";
 
 const deploy: DeployFunction = async ({ midl }) => {
+  await configure({
+    sinks: { console: getConsoleSink() },
+    loggers: [
+      { category: ["logtape", "meta"], sinks: [] },
+      { category: [], sinks: ["console"], lowestLevel: "trace" },]
+  });
+
   console.log("Starting deployment process...");
 
   await midl.initialize();
